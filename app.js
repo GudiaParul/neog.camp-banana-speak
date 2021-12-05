865d64a9b420c36a13ac8897c2c5b56a5574fc3e
Parul Gupta <guptaparul1002@gmail.com>
2021-12-05 12:23:28 +0530

    translate commit

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 
+console.log(txtInput)
 
-alert("script works")
+function clickHandler() {
+    console.log("clicked");
+    console.log("input",txtInput.value);
+}
+
+btnTranslate.addEventListener("click", clickHandler)
+
